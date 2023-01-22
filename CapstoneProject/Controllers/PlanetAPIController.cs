7f88f6d7dc0f1d2477a61cbaecc8510fc3dcbb84
Tülin Taşpınar <tulinttaspinar@gmail.com>
2023-01-22 23:14:38 +0300

    Added planet api

---
+++ CapstoneProject/Controllers/PlanetAPIController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using CapstoneProject.Models;
+using System.Linq;
+
+namespace CapstoneProject.Controllers
+{
+    public class PlanetAPIController : Controller
+    {
+        List<PlanetInfoViewList> planet = new List<PlanetInfoViewList>();
+        public async Task<IActionResult> Index()
+        {
+            //Gezegenler hakkında bilgi
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://planets-info-by-newbapi.p.rapidapi.com/api/v1/planet/list"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "12a214de2cmshc3823f8b62052c7p17b3e6jsnaf381c5f6f6d" },
+        { "X-RapidAPI-Host", "planets-info-by-newbapi.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                planet = JsonConvert.DeserializeObject<List<PlanetInfoViewList>>(body);
+            }
+            return View(planet.OrderBy(x=>x.planetOrder).ToList());
+        }
+    }
+}
---
+++ CapstoneProject/Models/PlanetInfoViewList.cs
@@ -0,0 +1,30 @@
+﻿namespace CapstoneProject.Models
+{
+    public class PlanetInfoViewList
+    {
+            public Basicdetail[] basicDetails { get; set; }
+            public string description { get; set; }
+            public int id { get; set; }
+            public Imgsrc[] imgSrc { get; set; }
+            public string key { get; set; }
+            public string name { get; set; }
+            public string planetOrder { get; set; }
+            public string source { get; set; }
+            public string wikiLink { get; set; }
+        
+
+        public class Basicdetail
+        {
+            public string mass { get; set; }
+            public string volume { get; set; }
+        }
+
+        public class Imgsrc
+        {
+            public string img { get; set; }
+            public string imgDescription { get; set; }
+        }
+
+
+    }
+}
